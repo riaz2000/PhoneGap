2ca92ff6fbf2b8887458d7b4eeedf596dff0b7c2
DESKTOP-5NBSE25\Riaz Hussain <riaz2000@yahoo.com>
2018-01-04 23:32:01 +0500

    wwklj

--- www/js/floor.js
+++ www/js/floor.js
@@ -324,7 +324,10 @@ function addListeners(div, img, resId, indexOfAppInApps){
 	
 	var lbl_slct = document.createElement("Label");
 	lbl_slct.id = "lbl_slct:"+indexOfAppInApps;
-	lbl_slct.style="position: absolute; right:3px; top:3px; ";
+	//lbl_slct.style="position: absolute; right:3px; top:3px; ";
+	lbl_slct.style.position="absolute";
+	lbl_slct.style.right="3px";
+	lbl_slct.style.top="3px";
 	lbl_slct.style.color = "green";
 	div.appendChild(lbl_slct);
 	
